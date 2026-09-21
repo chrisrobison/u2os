6f416b64d4777dbd4753a944c9ec2da72bdf5393
Christopher Robison <christopher.robison@gmail.com>
2026-09-20 19:03:05 -0700

    diagnostics: export sanitized bug bundles (#96)

--- server/api/routes/diagnostics.js
+++ server/api/routes/diagnostics.js
@@ -1,8 +1,16 @@
 import { sendJson } from '../router.js';
 import { buildDiagnostics } from '../../diagnostics/diagnostics.js';
+import { buildBugBundle } from '../../diagnostics/bug-bundle.js';
 
 export function registerDiagnosticsRoutes(router, dependencies) {
   router.get('/api/diagnostics', async (_req, res) => {
     sendJson(res, 200, buildDiagnostics(dependencies));
+  });
+  router.post('/api/diagnostics/bug-bundle', async (_req, res) => {
+    const stamp = new Date().toISOString().replaceAll(':', '-');
+    sendJson(res, 200, buildBugBundle(dependencies), {
+      'Content-Disposition': `attachment; filename="u2os-debug-${stamp}.json"`,
+      'Cache-Control': 'no-store',
+    });
   });
 }
